3ecf576a415e54825fc092c728c36ff2a253c609
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-06 13:47:40 +0100

    Partie 5 exercice 5

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+const buttonPink = document.getElementById("pinkButton");
+const buttonBlue = document.getElementById("blue");
+const buttonViolet = document.getElementById("violet");
+const buttonBold = document.getElementById("bold");
+const buttonItalic = document.getElementById("italic");
+const text = document.getElementById("paragraph");
+
+
+buttonPink.addEventListener('click', function() {
+    text.removeAttribute("class");
+    text.setAttribute("class", "pink");
+})
+
+buttonBlue.addEventListener('click', function() {
+   // text.removeAttribute("class");
+    text.setAttribute("class", "blue");
+})
+
+buttonViolet.addEventListener('click', function() {
+    text.removeAttribute("class");
+    text.setAttribute("class", "violet");
+})
+
+buttonBold.addEventListener('click', function() {
+    text.removeAttribute("class");
+    text.setAttribute("class", "bold");
+})
+
+buttonItalic.addEventListener('click', function() {
+    text.removeAttribute("class");
+    text.setAttribute("class", "italic");
+})
+
+// Algo couleur + effet
+
+// buttonPink.addEventListener('click', function () {
+//     if (text.classList == "blue") {
+//         text.classList.remove("blue");
+//     }
+//     if (text.classList == "violet") {
+//         text.classList.remove('violet');
+//     }
+//     text.classList.toggle("pink");
+// });
+
+// buttonBlue.addEventListener('click', function () {
+//     if (text.classList == "pink") {
+//         text.classList.remove("pink");
+//     }
+//     if (text.classList == "violet") {
+//         text.classList.remove('violet');
+//     }
+//     text.classList.toggle("blue");
+// });
+
+// buttonViolet.addEventListener('click', function () {
+//     if (text.classList == "pink") {
+//         text.classList.remove("pink");
+//     }
+//     if (text.classList == "blue") {
+//         text.classList.remove('blue');
+//     }
+//     text.classList.toggle("violet");
+// });
+
+// buttonItalic.addEventListener('click', function () {
+//     if (text.classList == "bold") {
+//         text.classList.remove("bold");
+//     }
+//     text.classList.toggle("italic");
+// });
+
+// buttonBold.addEventListener('click', function () {
+//     if (text.classList == "italic") {
+//         text.classList.remove("italic");
+//     }
+//     text.classList.toggle("bold");
+// });
